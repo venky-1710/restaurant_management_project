43dcdc0f5fe7afac3009554a0baa36a3af4f9383
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-04 20:03:46 +0530

    17462

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Menu
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+    search_fields = ('name',)
