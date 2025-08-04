56e59a3b7e3d06f6ca0c3a33d0071cd7c712c85e
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-04 19:52:39 +0530

    17461

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,11 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Menu(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
