f2d067074a9a3269c725eae7c28003d48313c731
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-04 17:58:47 +0530

    17458

--- home/views.py
+++ home/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import response
+
+class MenuAPIView(APIView):
+    def gets(self, request):
+        menu = [
+            {
+                "name" : "Pizza",
+                "description" : "Classic delight with 100% real mozzarella cheese",
+                "price" : 250
+            },
+            {
+                "name": "Paneer",
+                "description":"Chunks of paneer marinated in tandoori masala",
+                "price":180
+            }
+        ]
+        return Response(menu)
 
 # Create your views here.
 
