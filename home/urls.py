43f39a0d8c1a7e37a5f2f82ac36fc84484ca8c1d
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-08 22:40:09 +0530

    24399

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('', views.homepage, name='homepage'),
     path('menu/',MenuAPIView.as_view(),name='menu'),
     path('about/',views.about_page, name='about'),
 ]
