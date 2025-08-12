1ffd516f2440a5d8d0296f24b68b22fbfa235b4b
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-12 19:06:15 +0530

    29241

--- home/views.py
+++ home/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.conf import settings
-
-import requests
 from .models import MenuItem
 class MenuAPIView(APIView):
     def get(self, request):
