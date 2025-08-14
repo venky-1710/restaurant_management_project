cdd56f874ee76d1a4fc50eac714c6ac25584f908
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-14 23:35:40 +0530

    31315

--- home/models.py
+++ home/models.py
@@ -8,3 +8,18 @@ class Menu(models.Model):
 
     def __str__(self):
         return self.name
+
+
+from django.db import models
+
+class Feedback(models.Model):
+    comment = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Feedback #{self.id} - {self.submitted_at.strftime('%Y-%m-%d %H:%M')}"
+
+
+
+
+
