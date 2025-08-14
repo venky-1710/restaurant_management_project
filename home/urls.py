3b836ec21bf4d64ac6c0c9e306967125ebd5945e
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-14 23:43:27 +0530

    31315

--- home/urls.py
+++ home/urls.py
@@ -1,14 +1,21 @@
 
-
 from django.urls import path
 from . import views
-from .views import MenuAPIView  # ✅ Direct import
+from .views import MenuAPIView
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('menu/', MenuAPIView.as_view(), name='menu'),
     path('about/', views.about_page, name='about'),
     path('menu-list/', views.menu_list, name='menu_list'),
-    path('feedback/',views.feedback_view, name= 'feedback'),
-    path('feedback-success/',views.feedback_success_view, name='feedback_success'),
+    path('feedback/', views.feedback_view, name='feedback'),
+    path('feedback-success/', views.feedback_success_view, name='feedback_success'),
 ]
+
+
+
+
+
+
+
+
