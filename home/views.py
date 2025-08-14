bb26fe26d503fec6d573cb73a707a31489105773
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-14 23:31:32 +0530

    31315

--- home/views.py
+++ home/views.py
@@ -74,9 +74,16 @@ def home(request):
 
 
 
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
 
+def feedback_view(request):
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('feedback_success')  # Redirect after saving
+    else:
+        form = FeedbackForm()
 
-
-
-
-
+    return render(request, 'feedback.html', {'form': form})
