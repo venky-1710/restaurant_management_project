a1a0c5d1f5da5b0c34afd297bf92db82031f1ec4
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-14 23:40:35 +0530

    31315

--- home/urls.py
+++ home/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('menu/', MenuAPIView.as_view(), name='menu'),
     path('about/', views.about_page, name='about'),
     path('menu-list/', views.menu_list, name='menu_list'),
+    path('feedback/',views.feedback_view, name= 'feedback'),
+    path('feedback-success/',views.feedback_success_view, name='feedback_success'),
 ]
