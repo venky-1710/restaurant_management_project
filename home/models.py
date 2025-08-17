7373d58f39b8e0aa7d52e52eca48ad6c126c2280
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-17 18:46:10 +0530

    33125

--- home/models.py
+++ home/models.py
@@ -19,7 +19,13 @@ class Feedback(models.Model):
     def __str__(self):
         return f"Feedback #{self.id} - {self.submitted_at.strftime('%Y-%m-%d %H:%M')}"
 
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.name} ({self.email})"
 
 
 
