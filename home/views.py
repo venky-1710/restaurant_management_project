6120f2e713b0c3d5c67a4a1829fbe051d2e45210
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-17 18:59:30 +0530

    33128

--- home/views.py
+++ home/views.py
@@ -108,7 +108,9 @@ def home(request):
     restaurant_name = RestaurantInfo.objects.first()  # get first entry
     return render(request, 'home.html', {'restaurant_name': restaurant_name})
 
-
+def menu_view(request):
+    items = MenuItem.objects.all()  # fetch all items
+    return render(request, 'menu.html', {'items': items})
 
 
 
