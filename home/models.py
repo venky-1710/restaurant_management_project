216315f55d800b7373c58a8dadceb24b61196f9a
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-19 17:56:50 +0530

    34600

--- home/models.py
+++ home/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
-class Menu(models.Model):
-    name = models.CharField(max_length=100)
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)  # ✅
 
     def __str__(self):
         return self.name
-
 
 from django.db import models
 
