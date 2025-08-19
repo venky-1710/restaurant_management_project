f18ef2bb0419cda03beb175d297554188feb4c45
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-19 17:54:49 +0530

    34601

--- home/urls.py
+++ home/urls.py
@@ -1,21 +1,27 @@
 
-from django.urls import path
-from . import views
-from .views import MenuAPIView
+# from django.urls import path
+# from . import views
+# from .views import MenuAPIView
+
+# urlpatterns = [
+#     path('', views.homepage, name='homepage'),
+#     path('menu/', MenuAPIView.as_view(), name='menu'),
+#     path('about/', views.about_page, name='about'),
+#     path('menu-list/', views.menu_list, name='menu_list'),
+#     path('feedback/', views.feedback_view, name='feedback'),
+#     path('feedback-success/', views.feedback_success_view, name='feedback_success'),
+#     path("contact/",views.contact_view, name="contact"),
+# ]
+
+
+
+from django.urls import path   # ✅ import path
+from . import views            # ✅ import views from current app
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('menu/', MenuAPIView.as_view(), name='menu'),
-    path('about/', views.about_page, name='about'),
-    path('menu-list/', views.menu_list, name='menu_list'),
-    path('feedback/', views.feedback_view, name='feedback'),
-    path('feedback-success/', views.feedback_success_view, name='feedback_success'),
-    path("contact/",views.contact_view, name="contact"),
+    path('contact/', views.contact_view, name='contact'),
 ]
-
-
-
-
 
 
 
