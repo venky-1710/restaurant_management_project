d323c625ee46f478970cad21020788cd4bad43d1
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-19 18:17:53 +0530

    34598

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,9 @@
 
 from django.contrib import admin
 from .models import Menu
+from .models import Restaurant
 
+admin.site.register(Restaurant)
 @admin.register(Menu)
 class MenuAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
