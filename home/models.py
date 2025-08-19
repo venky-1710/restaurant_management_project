a204600f57cbbdad17fa4c73a0c96f5eaf409515
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-08-19 17:46:24 +0530

    34602

--- home/models.py
+++ home/models.py
@@ -47,7 +47,14 @@ class MenuItem(models.Model):
         return self.name
 
 
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f"{self.address}, {self.city}, {self.state} {self.zip_code}"
 
 
 
