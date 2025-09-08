db399857c9a3bdbb92d0b2a42b9710deabaadaa8
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-09-09 04:58:56 +0530

    43546

--- home/models.py
+++ home/models.py
@@ -91,4 +91,11 @@ class Feedback(models.Model):
 
 
 
+class About(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='about_images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
         
