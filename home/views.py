819ab6d70525d347d86eb0e5285c14624389a505
SURAMPUDI VENKATESWARARAO <venkysss47@gmail.com>
2025-09-09 04:55:17 +0530

    43547

--- home/views.py
+++ home/views.py
@@ -206,7 +206,18 @@ def order_confirmation(request):
 
 
 
+from django.shortcuts import render, redirect
+from .forms import FeedbackForm
 
+def feedback_view(request):
+    if request.method == "POST":
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'feedback_success.html')  # confirmation page
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback.html', {'form': form})
 
 
 
